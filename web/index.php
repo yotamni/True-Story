b30d87daeff21f8ca8176fe742386dad2151ce4f
Yotam Nitzan <yotamni@gmail.com>
2018-06-19 21:31:09 +0300

    irobot route bug fix

--- web/index.php
+++ web/index.php
@@ -1,25 +1,38 @@
-<?php
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TRUE STORY</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/echarts.min.js"></script>
+    <script src="js/fontawesome-all.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </head>
+  <body class="landingBody">
+    <div class="landingContainer">
+      <div class="col-lg-2 landingLogo">
+        <a href="home.php"><img src="img/TS-logo.png" class="col-lg-10 img-fluid" alt=""></a>
+      </div>
+      <div class="row">
+        <div class="col-lg-5 col-md-12 landing">
+          <img src="img/computer2.png" class="img img-fluid"alt="">
+        </div>
+        <div class="col-lg-5 col-md-12">
+          <h2>TRUE STORY analyzes Instagram user accounts and determine with a high certainty if its fake or real</h2>
+          <hr>
+          <h5>Our application gathers information and analyses it  in order to determine what stands behind a tested profile.</h5>
+          <a href="login.html">
+            <button type="button" class="btn btn-dark landingButton">LOG IN</button>
+          </a>
+          <a href="signUp.html">
+            <button type="button" class="btn btn-outline-dark landingButton">SIGN UP</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
